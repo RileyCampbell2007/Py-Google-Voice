5f9edfb102bf1a870f3f994b421899d87e05f817
RileyCampbell2007 <riley.j.campbell.2007@gmail.com>
2023-07-23 21:56:43 -0600

    Re-write

--- README.md
+++ README.md
@@ -17,33 +17,73 @@ You can install the necessary python dependencies using pip:
 pip install selenium undetected-chromedriver webdriver-manager
 ```
 
-Google Chrome can be downloaded from the [Google website.](https://chrome.google.com/)
+Google Chrome can be downloaded from the [Google website](https://chrome.google.com/).
 
 ## How to use
 
-Here is a short example script that demonstrates how to use Py Google Voice:
+Creating the Google Voice session is done like this:
 
 ```python
-import pygooglevoice as gv
+from pygooglevoice import GoogleVoice
 
-# Replace with your Google Voice email and password
-email = "youremail@gmail.com"
-password = "yourpassword"
+emailAddress = "example@example.com"
+password = "password"
 
-GVSession = gv.login(email, password)
-
-# Fetch all unread conversations
-unread_conversations = gv.check_for_unread_conversations(GVSession)
-
-# Read the latest message in the first unread conversation
-latest_message = gv.read_latest_message(GVSession, unread_conversations[0])
-
-print("Latest message: ", latest_message)
-
-# Send a new message to the same contact
-gv.send_message(GVSession, unread_conversations[0], "This is a test reply from Py Google Voice.")
+GVSession = GoogleVoice(emailAddress, password)
 ```
 
-Please note that you would need to replace `"youremail@gmail.com"` and `"yourpassword"` with your actual Google Voice email and password in order for this script to work.
+Please note that you would need to replace `"example@example.com"` and `"password"` with your actual Google Voice email and password in order for this script to work.
 
 2-Factor Authentication is a required step during login, Google will send a notification to your authentication device, you will need to allow the login attempt in order to use this script.
+
+If you want to run the browser in headless mode you can set `headless=True`.
+
+
+If you don't want to save the Chrome Profile you can set `saveChromeData=False`.
+
+If you want to change the Chrome Profile path you can set `chromeDataPath="path/to/profile"`.
+
+If you want to use Chromium instead of Google Chrome you can do:
+
+```python
+from pygooglevoice import GoogleVoice, browsers
+
+emailAddress = "example@example.com"
+password = "password"
+
+GVSession = GoogleVoice(emailAddress, password, browser=browsers.chromium)
+```
+
+Checking for conversations with unread messages is done like this:
+
+```python
+unreadConversations = GVSession.check_for_unread_conversations()
+```
+
+This will return a list with the conversationIDs of every conversation with unread messages.
+
+To send a message do:
+
+```python
+GVSession.send_message(conversationID, 'Message')
+```
+
+Sending with a photo is done like this:
+
+```python
+GVSession.send_message(conversationID, 'Message', photos=['path/to/photo.jpg'])
+```
+
+Reading the latest message in a conversation can be done like this:
+
+```python
+message = GVSession.read_latest_message(conversationID)
+```
+
+The Selenium webdriver can be accesed directly with `GVSession.chrome`.
+
+## To do
+
+* Allow for reading all unread messages instead of the latest message.
+* Add to PyPi so it can be installed via pip.
+
--- pygooglevoice.py
+++ pygooglevoice.py
@@ -15,155 +15,156 @@ class browsers:
     googleChrome = 'googleChrome'
     chromium = 'chromium'
 
-def login(email: str, password: str, saveChromeData: bool = True, chromeDataPath: str = 'google-chrome', headless: bool = True, browser = 'googleChrome'):
-    # Setting up Chrome options
-    chrome_options = ChromeOptions()
+class GoogleVoice:
+    def __init__(self, email: str, password: str, saveChromeData: bool = True, chromeDataPath: str = 'google-chrome', headless: bool = True, browser = 'googleChrome'):
+        # Setting up Chrome options
+        chrome_options = ChromeOptions()
 
-    # Check if headless mode is enabled
-    if headless:
-        chrome_options.add_argument("--headless")
+        # Check if headless mode is enabled
+        if headless:
+            chrome_options.add_argument("--headless")
 
-    # Check if saving data is enabled
-    if saveChromeData:
-        chrome_options.add_argument('--user-data-dir=' + chromeDataPath)
+        # Check if saving data is enabled
+        if saveChromeData:
+            chrome_options.add_argument('--user-data-dir=' + chromeDataPath)
 
-    # Additional Chrome options
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-blink-features")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.4;) AppleWebKit/535.17 (KHTML, like Gecko) Chrome/49.0.3318.292 Safari/600"')
+        # Additional Chrome options
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-blink-features")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.4;) AppleWebKit/535.17 (KHTML, like Gecko) Chrome/49.0.3318.292 Safari/600"')
 
-    # Initializing Chrome WebDriver with options
+        # Initializing Chrome WebDriver with options
 
-    if(browser == 'googleChrome'):
-        print(ChromeDriverManager().install())
-        service=ChromeService(ChromeDriverManager().install(), desired_capabilities={'unexpectedAlertBehaviour': 'ignore'})
-    elif(browser == 'chromium'):
-        service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), desired_capabilities={'unexpectedAlertBehaviour': 'ignore'})
+        if(browser == 'googleChrome'):
+            service=ChromeService(ChromeDriverManager().install(), desired_capabilities={'unexpectedAlertBehaviour': 'ignore'})
+        elif(browser == 'chromium'):
+            service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), desired_capabilities={'unexpectedAlertBehaviour': 'ignore'})
 
-    chrome = webdriver.Chrome(service=service, options=chrome_options)
+        self.chrome = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Execute JavaScript to hide WebDriver properties
-    chrome.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-        "source": """
-            Object.defineProperty(navigator, 'webdriver', {
-            get: () => undefined
-            })
-        """
-    })
+        # Execute JavaScript to hide WebDriver properties
+        self.chrome.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+                })
+            """
+        })
 
-    # Open Google login page
-    chrome.get('https://accounts.google.com/')
+        # Open Google login page
+        self.chrome.get('https://accounts.google.com/')
 
-    # Check if already logged in
-    if 'https://myaccount.google.com' in chrome.current_url:
-        return chrome
+        # Check if already logged in
+        if 'https://myaccount.google.com' in self.chrome.current_url:
+            return
 
-    # Wait for email input field to be present
-    emailBox = WebDriverWait(chrome, 120).until(EC.presence_of_element_located((By.ID, 'identifierId')))
+        # Wait for email input field to be present
+        emailBox = WebDriverWait(self.chrome, 120).until(EC.presence_of_element_located((By.ID, 'identifierId')))
 
-    # Enter email character by character
-    for i in range(len(email)):
-        char = email[i]
-        emailBox.send_keys(char)
-    emailBox.send_keys(Keys.ENTER)
+        # Enter email character by character
+        for i in range(len(email)):
+            char = email[i]
+            emailBox.send_keys(char)
+        emailBox.send_keys(Keys.ENTER)
 
-    # Wait for password input field to be present
-    passwordBox = WebDriverWait(chrome, 120).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="Passwd"]')))
+        # Wait for password input field to be present
+        passwordBox = WebDriverWait(self.chrome, 120).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[name="Passwd"]')))
 
-    # Enter password character by character
-    for i in range(len(password)):
-        char = password[i]
-        passwordBox.send_keys(char)
-    passwordBox.send_keys(Keys.ENTER)
+        # Enter password character by character
+        for i in range(len(password)):
+            char = password[i]
+            passwordBox.send_keys(char)
+        passwordBox.send_keys(Keys.ENTER)
 
-    verificationNeeded = True
-    while verificationNeeded:
-        try:
-            # Wait for 2-Step Verification page to appear
-            WebDriverWait(chrome, 1).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), '2-Step Verification'))
-            verificationTextBox = chrome.find_element(By.CLASS_NAME, 'PrDSKc')
-            verificationText = verificationTextBox.text.strip().replace('"', '').replace('<strong>', '').replace('</strong>', '')
-            print(f'2-Step Verification Required. {verificationText}')
+        verificationNeeded = True
+        while verificationNeeded:
+            try:
+                # Wait for 2-Step Verification page to appear
+                WebDriverWait(self.chrome, 1).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), '2-Step Verification'))
+                verificationTextBox = self.chrome.find_element(By.CLASS_NAME, 'PrDSKc')
+                verificationText = verificationTextBox.text.strip().replace('"', '').replace('<strong>', '').replace('</strong>', '')
+                print(f'2-Step Verification Required. {verificationText}')
 
-            verified = False
-            while not verified:
-                try:
-                    # Wait for 2-Step Verification to complete
-                    WebDriverWait(chrome, 1).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), '2-Step Verification'))
-                except TimeoutException:
-                    print('Verified!')
-                    verified = True
-        except TimeoutException:
-            # Check if redirected to account page after verification
-            if chrome.current_url.startswith('https://myaccount.google.com/'):
-                verificationNeeded = False
+                verified = False
+                while not verified:
+                    try:
+                        # Wait for 2-Step Verification to complete
+                        WebDriverWait(self.chrome, 1).until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), '2-Step Verification'))
+                    except TimeoutException:
+                        print('Verified!')
+                        verified = True
+            except TimeoutException:
+                # Check if redirected to account page after verification
+                if self.chrome.current_url.startswith('https://myaccount.google.com/'):
+                    verificationNeeded = False
+                    
+    def check_for_unread_conversations(self):
+        GVSession = self.chrome
+        if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
+            GVSession.get('https://voice.google.com/u/0/messages')
+        conversationsBox = WebDriverWait(WebDriverWait(GVSession,120).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'md-virtual-repeat-container[gv-id="thread-list-container"]'))),120).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="md-virtual-repeat-offsetter"]')))
+        conversationList = conversationsBox.find_elements(By.CSS_SELECTOR, 'gv-text-thread-item[conversation="conversation"]')
+        unreadConversations = []
+        for conversation in conversationList:
+            try:
+                conversationElement = conversation.find_element(By.CSS_SELECTOR, 'gv-thread-item').find_element(By.CSS_SELECTOR, 'div')
+            except:
+                continue
+            conversationText = conversationElement.get_attribute('aria-label')
+            if (conversationText.startswith('Unread. ')):
+                unreadConversations = [conversationElement.get_attribute('gv-thread-id')] + unreadConversations
+        return unreadConversations
 
-    return chrome
-
-def check_for_unread_conversations(GVSession):
-    if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
-        GVSession.get('https://voice.google.com/u/0/messages')
-    conversationsBox = WebDriverWait(WebDriverWait(GVSession,120).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'md-virtual-repeat-container[gv-id="thread-list-container"]'))),120).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="md-virtual-repeat-offsetter"]')))
-    conversationList = conversationsBox.find_elements(By.CSS_SELECTOR, 'gv-text-thread-item[conversation="conversation"]')
-    unreadConversations = []
-    for conversation in conversationList:
-        try:
-            conversationElement = conversation.find_element(By.CSS_SELECTOR, 'gv-thread-item').find_element(By.CSS_SELECTOR, 'div')
-        except:
-            continue
-        conversationText = conversationElement.get_attribute('aria-label')
-        if (conversationText.startswith('Unread. ')):
-            unreadConversations = [conversationElement.get_attribute('gv-thread-id')] + unreadConversations
-    return unreadConversations
-
-def read_latest_message(GVSession, conversationID):
-    if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
-        GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
-    else:
-        try:
-            GVSession.find_element(By.CSS_SELECTOR, f'div[gv-thread-id="{conversationID}"]').click()
-        except:
+    def read_latest_message(self, conversationID):
+        GVSession=self.chrome
+        if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
             GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
-    WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CLASS_NAME,'incoming')))
-    messageContainer = GVSession.find_elements(By.CLASS_NAME,'incoming')[-1]
-    message = messageContainer.find_element(By.TAG_NAME,'gv-annotation').text
-    return message
+        else:
+            try:
+                GVSession.find_element(By.CSS_SELECTOR, f'div[gv-thread-id="{conversationID}"]').click()
+            except:
+                GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
+        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CLASS_NAME,'incoming')))
+        messageContainer = GVSession.find_elements(By.CLASS_NAME,'incoming')[-1]
+        message = messageContainer.find_element(By.TAG_NAME,'gv-annotation').text
+        return message
 
-def send_message(GVSession, conversationID: str, message: str, photos: list = []):
-    if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
-        GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
-    else:
-        try:
-            GVSession.find_element(By.CSS_SELECTOR, f'div[gv-thread-id="{conversationID}"]').click()
-        except:
+    def send_message(self, conversationID: str, message: str, photos: list = []):
+        GVSession = self.chrome
+        if (not GVSession.current_url.startswith('https://voice.google.com/u/0/messages')):
             GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
-    if(len(photos) > 0):
-        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, 'ib1')))
-        GVSession.execute_script('document.getElementById("ib1").dispatchEvent(new MouseEvent("click", {view: window, bubbles: true, cancelable: true}));')
-        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CSS_SELECTOR,"iframe[allow=\"camera 'src' https://docs.google.com\"]")))
-        GVSession.switch_to.frame(GVSession.find_element(By.CSS_SELECTOR,"iframe[allow=\"camera 'src' https://docs.google.com\"]"))
-        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, ':7')))
-        GVSession.find_element(By.ID, ':7').click()
-        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CSS_SELECTOR,"input[accept='image/bmp,image/gif,image/jpeg,image/png,image/tiff']")))
-        for file in photos:
-            GVSession.find_element(By.CSS_SELECTOR,"input[accept='image/bmp,image/gif,image/jpeg,image/png,image/tiff']").send_keys(os.path.abspath(file))
-        class aria_disabled_not_true(object):
-            def __init__(self, locator):
-                self.locator = locator
+        else:
+            try:
+                GVSession.find_element(By.CSS_SELECTOR, f'div[gv-thread-id="{conversationID}"]').click()
+            except:
+                GVSession.get(f'https://voice.google.com/u/0/messages?itemId={urllib.parse.quote(conversationID)}')
+        if(len(photos) > 0):
+            WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, 'ib1')))
+            GVSession.execute_script('document.getElementById("ib1").dispatchEvent(new MouseEvent("click", {view: window, bubbles: true, cancelable: true}));')
+            WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CSS_SELECTOR,"iframe[allow=\"camera 'src' https://docs.google.com\"]")))
+            GVSession.switch_to.frame(GVSession.find_element(By.CSS_SELECTOR,"iframe[allow=\"camera 'src' https://docs.google.com\"]"))
+            WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, ':7')))
+            GVSession.find_element(By.ID, ':7').click()
+            WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.CSS_SELECTOR,"input[accept='image/bmp,image/gif,image/jpeg,image/png,image/tiff']")))
+            for file in photos:
+                GVSession.find_element(By.CSS_SELECTOR,"input[accept='image/bmp,image/gif,image/jpeg,image/png,image/tiff']").send_keys(os.path.abspath(file))
+            class aria_disabled_not_true(object):
+                def __init__(self, locator):
+                    self.locator = locator
 
-            def __call__(self, GVSession):
-                element = GVSession.find_element(*self.locator)
-                if element.get_attribute("aria-disabled") != "true":
-                    return element
-                else:
-                    return False
-        WebDriverWait(GVSession, 300).until(aria_disabled_not_true((By.ID, "picker:ap:2")))
-        GVSession.find_element(By.ID,'picker:ap:2').click()
-        GVSession.switch_to.default_content()
-    WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, 'input_0')))
-    GVSession.execute_script(f'document.getElementById("input_0").value=`{message}`;')
-    GVSession.find_element(By.ID, 'input_0').send_keys('a'+Keys.BACK_SPACE+Keys.ENTER)
+                def __call__(self, GVSession):
+                    element = GVSession.find_element(*self.locator)
+                    if element.get_attribute("aria-disabled") != "true":
+                        return element
+                    else:
+                        return False
+            WebDriverWait(GVSession, 300).until(aria_disabled_not_true((By.ID, "picker:ap:2")))
+            GVSession.find_element(By.ID,'picker:ap:2').click()
+            GVSession.switch_to.default_content()
+        WebDriverWait(GVSession, 120).until(EC.presence_of_element_located((By.ID, 'input_0')))
+        GVSession.execute_script(f'document.getElementById("input_0").value=`{message}`;')
+        GVSession.find_element(By.ID, 'input_0').send_keys('a'+Keys.BACK_SPACE+Keys.ENTER)
